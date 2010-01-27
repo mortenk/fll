4db58cd26b95800010add3aeeb361a2fe72c8bca
U-VSLANN00\vsasmkn <vsasmkn@vswsmkn.vps.no>
2010-01-27 12:57:47 +0100

    Need unique ids

--- src/no/fll/web/ActivityService.java
+++ src/no/fll/web/ActivityService.java
@@ -4,6 +4,6 @@ package no.fll.web;
 public class ActivityService {
 
 	public Activity[] getActivities() {
-		return new Activity[]{new Activity(1, "Teori", 15), new Activity(1, "Teknisk", 15), new Activity(1, "Presentasjon", 15)};
+		return new Activity[]{new Activity(1, "Teori", 15), new Activity(2, "Teknisk", 15), new Activity(3, "Presentasjon", 15)};
 	}
 }
