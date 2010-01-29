c2f37ad0bd2f678bd613b38879e727c9e4439ad2
U-VSLANN00\vsasmkn <vsasmkn@vswsmkn.vps.no>
2010-01-29 15:57:32 +0100

    Added hibernate mapping

--- src/main/java/no/fll/activity/Activity.java
+++ src/main/java/no/fll/activity/Activity.java
@@ -1,11 +1,29 @@
 package no.fll.activity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ACTIVITY")
 public class Activity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ACTIVITY_ID")
 	private int id;
+	
+	@Column(name = "ACTIVITY")
 	private String activity;
+
+	@Column(name = "DURATION")
 	private int duration;
 
+	public Activity() {}
+	
 	public Activity(int id, String activity, int duration) {
 		super();
 		this.id = id;
