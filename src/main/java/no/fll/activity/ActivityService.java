609edcae9fdf254166b79e423efdc4d4ccb11024
U-VSLANN00\vsasmkn <vsasmkn@vswsmkn.vps.no>
2010-01-29 15:58:00 +0100

    Added dummy methods for crud

--- src/main/java/no/fll/activity/ActivityService.java
+++ src/main/java/no/fll/activity/ActivityService.java
@@ -6,4 +6,16 @@ public class ActivityService {
 	public Activity[] getActivities() {
 		return new Activity[]{new Activity(1, "Teori", 15), new Activity(2, "Teknisk", 15), new Activity(3, "Presentasjon", 15)};
 	}
+
+	public Activity[] createActivity(Activity[] activities) {
+		return getActivities();
+	}
+
+	public Activity[] updateActivity(Activity[] activities) {
+		return getActivities();
+	}
+
+	public Activity[] deleteActivity(Activity[] activities) {
+		return getActivities();
+	}
 }
