3537db549f7b65f701106e781b9ed64e1ea1ec89
Morten Knudsen <goatbrain@gmail.com>
2010-12-01 10:17:43 +0100

    Fixed service mock

--- src/main/webapp/dwr/interface/PlanService.js
+++ src/main/webapp/dwr/interface/PlanService.js
@@ -1,14 +1,14 @@
 var PlanService = {};
 PlanService.dummyData = {
 	objectsToConvertToRecords: [ 
-		{id: 1, time: '09.00', team: 'Lag 1', activity: 'Pit'},
-		{id: 2, time: '09.06', team: 'Lag 1', activity: 'Ring'},
-		{id: 3, time: '09.20', team: 'Lag 1', activity: 'Teori'},
-		{id: 4, time: '10.00', team: 'Lag 1', activity: 'Teknisk'},
-		{id: 5, time: '09.06', team: 'Lag 2', activity: 'Pit'},
-		{id: 6, time: '09.12', team: 'Lag 2', activity: 'Ring'},
-		{id: 7, time: '10.20', team: 'Lag 2', activity: 'Teori'},
-		{id: 8, time: '11.00', team: 'Lag 2', activity: 'Teknisk'}
+		{id: 1, time: '09.00', team: {id: 1, number: 1, name: 'Lag 1'}, activity: 'Pit'},
+		{id: 2, time: '09.06', team: {id: 1, number: 1, name: 'Lag 1'}, activity: 'Ring'},
+		{id: 3, time: '09.20', team: {id: 1, number: 1, name: 'Lag 1'}, activity: 'Teori'},
+		{id: 4, time: '10.00', team: {id: 1, number: 1, name: 'Lag 1'}, activity: 'Teknisk'},
+		{id: 5, time: '09.06', team: {id: 2, number: 2, name: 'Lag 2'}, activity: 'Pit'},
+		{id: 6, time: '09.12', team: {id: 2, number: 2, name: 'Lag 2'}, activity: 'Ring'},
+		{id: 7, time: '10.20', team: {id: 2, number: 2, name: 'Lag 2'}, activity: 'Teori'},
+		{id: 8, time: '11.00', team: {id: 2, number: 2, name: 'Lag 2'}, activity: 'Teknisk'}
 	], 
 	success: true
 };
