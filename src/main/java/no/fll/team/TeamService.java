388bdb65462d79a2103ec0407f7e21936c9a67f5
Morten Knudsen <goatbrain@gmail.com>
2011-11-16 11:42:05 +0100

    Added generic dao class to isolate hibernate code

--- src/main/java/no/fll/team/TeamService.java
+++ src/main/java/no/fll/team/TeamService.java
@@ -2,37 +2,31 @@ package no.fll.team;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate3.HibernateTemplate;
-
+import no.fll.dao.CrudDAO;
 import no.fll.web.JsonReaderResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TeamService {
 
 	@Autowired
-	private HibernateTemplate hibernateTemplate;
-	
+	private CrudDAO dao;
+
 	public JsonReaderResponse<Team> getTeams() {
-		return new JsonReaderResponse<Team>(hibernateTemplate.loadAll(Team.class));
+		return dao.list(Team.class);
 	}
 
-	public JsonReaderResponse<Team> createTeam(List<Team> activities) {
-		try {
-			hibernateTemplate.saveOrUpdateAll(activities);
-			return new JsonReaderResponse<Team>(activities);
-		}
-		catch (RuntimeException ex) {
-			return new JsonReaderResponse<Team>();
-		}
+	public JsonReaderResponse<Team> createTeam(List<Team> teams) {
+		return dao.create(teams);
 	}
 
-	public JsonReaderResponse<Team> updateTeam(List<Team> oldValues, List<Team> newValues) {
-		return createTeam(newValues);
+	public JsonReaderResponse<Team> updateTeam(List<Team> oldValues,
+			List<Team> newValues) {
+		return dao.update(oldValues, newValues);
 	}
 
-	public JsonReaderResponse<Team> deleteTeam(List<Team> activities) {
-		hibernateTemplate.deleteAll(activities);
-		return new JsonReaderResponse<Team>(activities);
+	public JsonReaderResponse<Team> deleteTeam(List<Team> teams) {
+		return dao.deleteAll(teams);
 	}
 
 }
